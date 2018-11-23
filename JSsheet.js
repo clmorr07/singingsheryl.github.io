6d4481837bc8241e435879662ace6f174f67f219
Courtney Morrison <clmorr07@icloud.com>
2018-11-23 17:24:56 -0500

    update

--- JSsheet.js
+++ JSsheet.js
@@ -23,6 +23,8 @@ function myFunction() {
         x.className = "navbar";
     }
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -33,7 +35,8 @@ function scrollFunction() {
     }
 }
 
+// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
